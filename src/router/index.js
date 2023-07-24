1644c4567993f8088add24f397ba697a1b95016e
David Mason <david@moodo.com>
2023-07-24 09:56:27 +0100

    New preflop page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Analyzer from '@/views/Analyzer.vue'
+import Preflop from '@/views/Preflop.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/analyzer',
       name: 'analyzer',
       component: Analyzer
+    },
+    {
+      path: '/preflop',
+      name: 'preflop',
+      component: Preflop
     }
   ]
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans]
+      },
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        13: 'repeat(13, 40px)'
+      }
+    }
   },
   plugins: []
 }
